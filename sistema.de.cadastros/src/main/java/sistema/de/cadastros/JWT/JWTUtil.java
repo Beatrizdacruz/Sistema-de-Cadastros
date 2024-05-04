718cd6b14f50b7ff56f16521abb5afc682f3d4f1
Beatriz Cruz <83562801+Beatrizdacruz@users.noreply.github.com>
2024-05-04 17:39:02 -0300

    Delete sistema.de.cadastros directory

--- sistema.de.cadastros/src/main/java/sistema/de/cadastros/JWT/JWTUtil.java
+++
@@ -1,7 +0,0 @@
-package sistema.de.cadastros.JWT;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class JWTUtil {
-}
--- sistema.de.cadastros/src/main/java/sistema/de/cadastros/constents/cadastroConstants.java
+++
@@ -1,7 +0,0 @@
-package sistema.de.cadastros.constents;
-
-public class cadastroConstants {
-    public static final String SOMETHING_WENT_WRONG = "Something went wrong";
-
-    public static final String INVALID_DATA = "INVALID DATA.";
-}
