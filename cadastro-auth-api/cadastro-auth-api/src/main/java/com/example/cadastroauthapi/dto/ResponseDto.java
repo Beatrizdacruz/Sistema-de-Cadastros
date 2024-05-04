f5f3891e196e5fe0185ecf9b4a418bab2682ceee
Beatriz Cruz <83562801+Beatrizdacruz@users.noreply.github.com>
2024-05-04 17:39:44 -0300

    Add files via upload

---
+++ cadastro-auth-api/cadastro-auth-api/src/main/java/com/example/cadastroauthapi/dto/ResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.cadastroauthapi.dto;
+
+public record ResponseDto (String name, String token){
+}
---
+++ cadastro-auth-api/cadastro-auth-api/src/test/java/com/example/cadastroauthapi/CadastroAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastroauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
