15127ea56d8b7ff3f4e71de1b95813dc01225e9e
Beatriz Cruz <83562801+Beatrizdacruz@users.noreply.github.com>
2024-05-11 18:50:25 -0300

    Update query

--- cadastro-auth-api/src/main/java/com/example/cadastroauthapi/Repositories/AddressRepository.java
+++ cadastro-auth-api/src/main/java/com/example/cadastroauthapi/Repositories/AddressRepository.java
@@ -1,7 +1,13 @@
 package com.example.cadastroauthapi.Repositories;
 
-import com.example.cadastroauthapi.domain.endereco.Address;
+import com.example.cadastroauthapi.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, String> {
+    @Query("SELECT a FROM Address a WHERE a.user_id = ?1")
+    public List<Address> findAddressesByUserId(String user_id);
 }
